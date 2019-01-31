9f0f1342762521e6f5fd263469de465ba0d2086b
suzn89 <ssostory@gmail.com>
2019-01-31 17:35:19 +0900

    분기처리

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 $(function() {
+
+  // 전체 드롭다운 메뉴
   $('.onedepth').on({
     "mouseenter" : function(){
-      $('#header_wrap').stop().animate({"height":"330px"},600);
+      $('#header_wrap').stop().animate({"height":"330px"},300);      
     },
     "mouseleave" : function(){
-      $('#header_wrap').stop().animate({"height":"80px"},600);
+      $('#header_wrap').stop().animate({"height":"80px"},300);      
     }
   });
+
+  // 메뉴 상단 고정
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop >=100){
+      $('#header_wrap').addClass('sticky');
+    }else{
+      $('#header_wrap').removeClass('sticky');
+    }
+  });
+  
 });
